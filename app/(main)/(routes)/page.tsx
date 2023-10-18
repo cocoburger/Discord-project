f3e87bba56a995b2ecacabba89b18a7670bd14aa
burger <fsm15@naver.com>
2023-10-18 23:45:44 +0900

    DELETE: DARK MODE 기능 추가

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,13 @@
+import {UserButton} from '@clerk/nextjs';
+import {ModeToggle} from '@/components/mode-toggle';
+
 const state = true;
 
 export default function Home() {
-    return <div>This is a protected route.</div>;
+    return (
+        <div>
+            <UserButton afterSignOutUrl='/' />
+            <ModeToggle />
+        </div>
+    );
 }
