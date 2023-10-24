6202cf8726b391c4464b250665fd2c08f28b6e9e
burger <fsm15@naver.com>
2023-10-25 00:41:33 +0900

    FEATURE: 초기 모달 화면 완료

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -6,9 +6,14 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
 } from '@/components/ui/dialog';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {Button} from '@/components/ui/button';
+import {useEffect, useState} from 'react';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -20,6 +25,7 @@ const formSchema = z.object({
 });
 
 export const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -33,6 +39,14 @@ export const InitialModal = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
     };
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
     return (
         <Dialog open>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
@@ -45,6 +59,46 @@ export const InitialModal = () => {
                         change it later
                     </DialogDescription>
                 </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+                        <div className='space-y-8 px-6'>
+                            <div className='flex items-center justify-center text-center'>
+                                TODO: Image Upload
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name='name'
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className='uppercase text-xs font-bold text-zinc-500
+                                        dark:text-secondary/70
+                                        '
+                                        >
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className='bg-zinc-300/50 border-0
+                                                focus-visible:ring-0 text-black
+                                                focus-visible:ring-offset-0'
+                                                placeholder='Enter server name'
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage>Server name is reuired.</FormMessage>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className='bg-gray-100 px-6 py-4 '>
+                            <Button variant='primary' disabled={isLoading}>
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     );
